61840093193c51a938a46955a999d89380a52b45
AdibaMohammed <adobi.355@gmail.com>
2020-11-17 00:35:44 +0300

    كتابةأوامر الجافاسكربت +فصل أوامر(css عن صفحة html2) +كتابة كود من الصفحة3-5

--- html+css/JS1.js
+++ html+css/JS1.js
@@ -1,6 +1,6 @@
-function myFunction() {
-  var copyText = document.getElementById("myInput");
-  copyText.select();
+function myFunction(element) {
+  var copyText = document.getElementById(element);
+  copyText.select(element);
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
   alert("Copied the text: " + copyText.value);
