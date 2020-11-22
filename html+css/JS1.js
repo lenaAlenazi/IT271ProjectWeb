bd85da639c4505d59371785ce295281d73222e7a
AdibaMohammed <adobi.355@gmail.com>
2020-11-22 23:47:16 +0300

    زر اللايك

--- html+css/JS1.js
+++ html+css/JS1.js
@@ -1,7 +1,15 @@
-function myFunction(element) {
-  var copyText = document.getElementById(element);
-  copyText.select(element);
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  alert("Copied the text: " + copyText.value);
+function likeFunction(x,y){
+  var  count= 0;
+var image = document.getElementById(x);
+if(image.src.match("likeOff.png")){
+image.src="likOn2.png";
+count+=1;
+document.getElementById(y).innerHTML = count;
+}else{
+image.src= "likeOff.png";
+ if(count!=0){
+   count-=1;
+  document.getElementById(y).innerHTML = count;
+}else{document.getElementById(y).innerHTML = count;}
+}
 }
