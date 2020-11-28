45e1b19229c92c243bd6d18775c179b1cff6e0d4
AdibaMohammed <adobi.355@gmail.com>
2020-11-28 20:00:21 +0300

    حذف العداد

--- Project/JS1.js
+++ Project/JS1.js
@@ -3,13 +3,7 @@ function likeFunction(x,y){
 var image = document.getElementById(x);
 if(image.src.match("likeOff.png")){
 image.src="likOn2.png";
-count+=1;
-document.getElementById(y).innerHTML = count;
 }else{
 image.src= "likeOff.png";
- if(count!==0){
-   count-=1;
-  document.getElementById(y).innerHTML = count;
-}else{document.getElementById(y).innerHTML = count;}
 }
 }
