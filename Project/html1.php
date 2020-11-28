d369c3adcc63789936c810456d4fe7e8a1490f83
lenaAlenazi <lenaalenazi00@gmail.com>
2020-11-28 23:09:32 +0300

    مصدر زر التسجيل

--- Project/html1.php
+++ Project/html1.php
@@ -26,6 +26,7 @@
 <li><a href="html5.html">عبارات منوعة</a></li>
 <li><a href="page.php">اقتباسات الزوار</a></li>
 </ul>
+<!--  code from https://stackoverflow.com/questions/2906582/how-to-create-an-html-button-that-acts-like-a-link#:~:text=The%20plain%20HTML%20way%20is,URL%20in%20the%20action%20attribute.&text=If%20necessary%2C%20set%20CSS%20display,type%3D%22submit%22%3E%20. -->
 <a class="singinn" href="http://localhost/iT271ProjectWeb/Project/singin.php" target="_blank"><button class="singinn"> التسجيل </button></a>
 
 </div>
